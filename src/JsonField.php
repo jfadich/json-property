5f116a75533c1caa561684cbdf998a353829c4d1
John Fadich <jofad4@gmail.com>
2015-12-04 18:17:36 -0800

    adding trait/model

--- src/JsonField.php
+++ src/JsonField.php
@@ -1,8 +1,144 @@
 <?php
 
-namespace Jfadich;
+namespace Jfadich\JsonField;
+
+use Exception;
 
 class JsonField
 {
+    /**
+     * Model to present from
+     *
+     * @var Model
+     */
+    protected $model;
 
+    /**
+     * Field on the model to save data to
+     *
+     * @var string
+     */
+    private $field;
+
+    /**
+     * Array of current values
+     *
+     * @var array
+     */
+    protected $settings = [ ];
+
+    /**
+     * @param Model $model
+     * @param string $field
+     * @throws Exception
+     */
+    public function __construct( &$model, $field )
+    {
+        if ( !property_exists( $model, $field ) ) {
+            throw new Exception( 'json_field is not set.' );
+        }
+
+        $this->field    = $field;
+        $this->settings = $model->{$field} ?: [];
+        $this->model    = $model;
+    }
+
+    /**
+     * Merge the given array into existing values. Do not allow adding fields here
+     *
+     * @param array $attributes
+     */
+    public function merge( array $attributes )
+    {
+        $this->settings = array_merge(
+            $this->settings,
+            array_only( $attributes, array_keys( $this->settings ) )
+        );
+
+        $this->persist();
+    }
+
+    /**
+     * @param $key
+     * @param string $default
+     * @return mixed
+     */
+    public function get( $key, $default = null )
+    {
+        return array_get( $this->settings, $key, $default );
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function set( $key, $value )
+    {
+        $this->settings[ $key ] = $value;
+        $this->persist();
+    }
+
+    /**
+     * Remove element from settings array
+     *
+     * @param $key
+     */
+    public function forget($key)
+    {
+        if($this->has($key)) {
+            unset($this->settings[$key]);
+        }
+
+        $this->persist();
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function has( $key )
+    {
+        return array_key_exists( $key, $this->settings );
+    }
+
+    /**
+     * @return array|mixed
+     */
+    public function all()
+    {
+        return $this->settings;
+    }
+
+    /**
+     * Save the model
+     */
+    protected function persist()
+    {
+        $this->model->{$this->field} = $this->settings;
+
+        $this->model->save();
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __get( $key )
+    {
+        if ( $this->has( $key ) ) {
+            return $this->get( $key );
+        }
+
+        throw new Exception( "The property {$key} does not exist." );
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function __set( $key, $value )
+    {
+        $this->set( $key, $value );
+    }
 }
--- src/JsonFieldTrait.php
+++ src/JsonFieldTrait.php
@@ -2,7 +2,52 @@
 
 namespace Jfadich;
 
-trait JsonField
+trait JsonFieldTrait
 {
+    /**
+     * Instance of the Json Field object
+     * @var null
+     */
+    private $jsonInstance = null;
 
+    /**
+     * Get Json Object. If key is given return associated value
+     *
+     * @param null $key
+     * @param null $default
+     * @return mixed|null|Settings
+     * @throws Exception
+     */
+    public function getJson( $key = null, $default = null )
+    {
+        $instance = $this->getJsonInstance();
+
+        if ( $key !== null ) {
+            return $instance->get( $key, $default );
+        }
+
+        return $instance;
+    }
+
+    protected function getJsonInstance()
+    {
+        if ( $this->json_field === null ) {
+            throw new Exception( 'json_field is not set on model.' );
+        }
+
+        if ( !$this->jsonInstance instanceof JsonField ) {
+            $this->jsonInstance = new JsonField( $this, $this->json_field );
+        }
+
+        return $this->jsonInstance;
+    }
+
+    public function __call($method, $arguments)
+    {
+        // Defer to parent if the field is not set or is not being requested
+        if($this->json_field === null || $method != $this->json_field)
+            return parent::__call($method, $arguments);
+
+        return call_user_func_array([$this, 'getJson'], $arguments);
+    }
 }
